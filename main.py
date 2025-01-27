a59645c8064b651d1dc8d271ef31ecc9771be9a9
Jose Luis Villaronga <149181653+JoseLVillaronga@users.noreply.github.com>
2025-01-27 12:41:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -270,7 +270,7 @@ async def get_audio_devices():
             'input_devices': input_devices,
             'output_devices': output_devices,
             'virtual_mic': {
-                'name': 'Virtual_Microphone_Input',
+                'name': 'virtual_mic',
                 'description': None,
                 'source_name': None
             }
